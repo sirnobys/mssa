cdf2e28ec9887d35797824e67965938021130a31
Sarbeng <sarbengbaafi@gmail.com>
2020-10-12 09:46:04 +0000

    Done

--- app.js
+++ app.js
@@ -51,6 +51,7 @@ app.use('/users', usersRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
+  //res.render('404.ejs');
 });
 
 // error handler
@@ -61,7 +62,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('404.ejs');
 });
 
 module.exports = { app: app, server: server }; 
